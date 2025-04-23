78f8444dafc896748e3cc8424057420a35364ee2
ElreyInhumano <mqgareth@gmail.com>
2025-04-22 21:45:22 -0500

    Cambio en LimaCity
    Matías Quintanilla

--- LimaCity.cs
+++ LimaCity.cs
@@ -9,10 +9,12 @@ namespace Progra1251
     class LimaCity : City
     {
         public LimaCity(string name, float taxes, bool building1, bool building2,
-            bool building3, bool park1, bool park2, bool park3) : base(name, taxes, building1, building2,
-            building3, park1, park2, park3)
+            bool building3, bool park1, bool park2, bool park3) : base(name, taxes, building1, park1)
         {
-
+            this.building2 = building2;
+            this.building3 = building3;
+            this.park2 = park2;
+            this.park3 = park3;
         }
     }
 }
