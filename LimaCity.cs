a22260fa2955594e28291515a2bbf8cc166ccabc
ElreyInhumano <mqgareth@gmail.com>
2025-04-22 21:25:14 -0500

    creada clase LimaCity
    Matías Quintanilla

---
+++ LimaCity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra1251
+{
+    class LimaCity : City
+    {
+        public LimaCity(string name, float taxes, bool building1, bool building2,
+            bool building3, bool park1, bool park2, bool park3) : base(name, taxes, building1, building2,
+            building3, park1, park2, park3)
+        {
+
+        }
+    }
+}
