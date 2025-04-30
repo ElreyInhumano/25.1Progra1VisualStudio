4e5ac793bf9f482ec8cfb3fc95e47b109acab41f
ElreyInhumano <mqgareth@gmail.com>
2025-04-30 18:21:39 -0500

    creada clase cuadrado con sus funciones
    Matías Quintanilla

---
+++ Square.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra1251
+{
+    class Square: Rectangle, IArea
+    {
+        private Square(string name, int b, int h): base(name, b, h)
+        {
+        }
+
+        protected override string GetDetails()
+        {
+            return $"La lado de {name} es {b}, y su área es {Area()}";
+        }
+        new public float Area()
+        {
+            return b * b;
+        }
+    }
+}
