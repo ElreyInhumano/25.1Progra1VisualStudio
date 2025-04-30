bf6da2d6a5d5b3a24a7071832fda4b2e30aca77f
ElreyInhumano <mqgareth@gmail.com>
2025-04-30 17:39:06 -0500

    creada clase rectangulo

---
+++ Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra1251
+{
+    class Rectangle: Shape
+    {
+
+        protected int b, h;
+
+        protected Rectangle(string name, int b, int h) : base(name)
+        {
+            this.b = b;
+            this.h = h;
+        }
+
+        protected override string GetDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
